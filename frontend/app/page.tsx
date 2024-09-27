d0c89ad3d0bdcfea625882e82ffdc9784ee0e470
Andr3sPA <andres.pena2@udea.edu.co>
2024-09-27 14:03:58 -0500

    si

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,13 @@
 'use client'
-
 import React, { useEffect } from "react"
 import { useRouter } from "next/navigation"
-import { Button } from "@/components/ui/button"
+import {DataTableDemo} from '../components/table';
 
 export default function Home() {
   const router = useRouter()
-
   return (
     <div>
+      <DataTableDemo />
     </div>
   )
 }
