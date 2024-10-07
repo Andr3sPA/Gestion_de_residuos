0f23dfeaad664c62674fb43b43f0d64540ade2c4
Andr3sPA <andres.pena2@udea.edu.co>
2024-10-07 00:29:11 -0500

    fix endpoint list notifications

--- app/api/notifications/list/route.ts
+++ app/api/notifications/list/route.ts
@@ -29,12 +29,14 @@ export async function GET(req: NextRequest) {
             companySellerId: user.companyId,  
             },  
             type: 'auction_has_new_offer',
+            read:false
         },  
         {  
             offer: {  
             companyBuyerId: user.companyId,  
             },  
             type: 'offer_status_changed',
+            read:false
         },  
         ],  
     },  
