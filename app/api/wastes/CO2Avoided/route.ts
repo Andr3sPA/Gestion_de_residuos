d956c5ab4d5d88b0438aa92939e380c6d10c3071
Andr3sPA <andres.pena2@udea.edu.co>
2024-11-14 16:55:09 -0500

    add endpoint CO2 avoided

---
+++ app/api/wastes/CO2Avoided/route.ts
@@ -0,0 +1,43 @@
+import { prismaClient } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { unauthorized } from "../../(utils)/responses";
+
+
+export async function GET(req: NextRequest) {
+  const token = await getToken({ req })
+
+
+  if (!token || !token.sub) {
+    return unauthorized()
+  }
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: parseInt(token.sub),
+    },
+  })
+
+  if (!user) return unauthorized()
+  if (!user.companyId) return unauthorized("El usuario no pertenece a ninguna empresa")
+  const CO2Avoided: any[] = []
+  const wasteTypes = await prismaClient.wasteType.findMany();
+  if (!wasteTypes) return NextResponse.json({ error: "internal error" }, { status: 500 })
+  for (let i = 0; i < wasteTypes.length; i++) {
+    const wasteSum = await prismaClient.waste.aggregate({
+        where: { 
+          companyOwnerId: user?.companyId, 
+          wasteTypeId: wasteTypes[i].id
+        },
+        _sum: {
+          units: true
+        }
+      })
+    CO2Avoided.push({
+        name: wasteTypes[i].name,
+        avoidedCO2: (Number(wasteSum._sum.units) ?? 0) * Number(wasteTypes[i].emissionFactor)
+      });
+      
+  }
+
+  return NextResponse.json(CO2Avoided, { status: 201 })
+}
